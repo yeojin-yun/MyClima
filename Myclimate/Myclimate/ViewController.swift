bf672b1d80566e7d5979064861d5cdb71681ac8b
순진이 <sail041@naver.com>
2022-01-30 11:38:15 +0900

    SetUI

---
+++ Myclimate/Myclimate/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Myclimate
+//
+//  Created by 순진이 on 2022/01/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let searchBtn = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
